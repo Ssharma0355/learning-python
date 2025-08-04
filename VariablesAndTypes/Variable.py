c9f050def9a88e77bd9805daa15bd1c22b5c3c38
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-04 09:21:35 +0530

    variable

---
+++ VariablesAndTypes/Variable.py
@@ -0,0 +1,17 @@
+# Python is completely object oriented, and not "statically typed".
+#  You do not need to declare variables before using them, or declare their type. 
+# Every variable in Python is an object.
+
+# Python supports two types of numbers - integers(whole numbers) and 
+# floating point numbers(decimals). (It also supports complex numbers, 
+
+myint = 7
+print(myint)
+
+myVar = 7.0
+
+print(myVar)
+
+# to make int into float 
+myint = float(myint)
+print(myint)
