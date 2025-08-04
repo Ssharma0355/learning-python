c604854da2d2cc5be32a89864150ac6daebed399
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-04 09:40:38 +0530

    list

---
+++ List/List.py
@@ -0,0 +1,17 @@
+# Lists are very similar to arrays. 
+# They can contain any type of variable, and they can contain as many variables as you wish. 
+# Lists can also be iterated over in a very simple manner. Here is an example of how to build a list.
+
+myList =[]
+myList.append(1)
+myList.append(2)
+myList.append(3)
+
+# to print the index 
+print(myList[0])
+
+# to print the whole list with use of for loop 
+
+for x in myList:
+    print(x)
+
