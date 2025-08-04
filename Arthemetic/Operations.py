792e28a8e35a32ffd1b0f2c795c63f4d289d6fc5
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-04 09:57:43 +0530

    arthemic operations

---
+++ Arthemetic/Operations.py
@@ -0,0 +1,21 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+# Python supports concatenating strings using the addition operator:
+
+helloWorld = "Hellow" + " " + "World"
+print(helloWorld)
+
+# Python also supports multiplying strings to form a string with a repeating sequence:
+
+multipylingString = "Sachin" * 5
+print(multipylingString)
+
