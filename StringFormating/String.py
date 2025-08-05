9c3502133aeb6c7a68900ef261f153c3527afdd0
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-05 09:33:18 +0530

    string formating

--- StringFormating/String.py
+++ StringFormating/String.py
@@ -13,3 +13,17 @@ print("%s is %d years old"%(name, age))
 myList = [1,2,3]
 print("The list is %s" % myList)
 
+# %s - String (or any object with a string representation, like numbers)
+
+# %d - Integers
+
+# %f - Floating point numbers
+
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+# %x/%X - Integers in hex representation (lowercase/uppercase)
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)
