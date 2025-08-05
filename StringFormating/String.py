21529ca9ef66eba9829c94e586b75d9e617fcf9a
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-05 09:29:08 +0530

    %s string to print list

--- StringFormating/String.py
+++ StringFormating/String.py
@@ -1,3 +1,15 @@
+# Python uses C-style string formatting to create new, formatted strings. 
+# The "%" operator is used to format a set of variables enclosed in a "tuple" (a fixed size list), 
+# together with a format string, which contains normal text together with "argument specifiers",
+#  special symbols like "%s" and "%d".
+
 name = "Sachin"
 age =24
 print("%s is %d years old"%(name, age))
+
+# Any object which is not a string can be formatted using the %s operator as well. 
+# The string which returns from the "repr" method of that object is formatted as the string. For example:
+
+myList = [1,2,3]
+print("The list is %s" % myList)
+
