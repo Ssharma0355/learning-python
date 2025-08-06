7777a9e73fecc505aa0c17b814ab96790d177a43
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-06 07:40:24 +0530

    star and end

--- StringFormating/ReverseString.py
+++ StringFormating/ReverseString.py
@@ -12,3 +12,10 @@ array = [1,2,3,4,5]
 print(array[::-1])
 
 # reverse an array 
+
+
+sampleString = "Sachin GOAT"
+print(sampleString.startswith("Sachin")) 
+# it checks if the string starts with the same string  
+print(sampleString.endswith("Bakri"))
+#  it checks if the string ends with the same string 
