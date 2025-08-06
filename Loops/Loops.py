4315d1ab436383bfd72b28ca580c61536586b8c8
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-06 08:27:29 +0530

    loops

---
+++ Loops/Loops.py
@@ -0,0 +1,18 @@
+primes = [2,3,4,4,5]
+
+for prime in primes:
+    print(prime)
+
+array = [1,2,3,4,5,6,7,8]
+for x in array:
+    print("Array", x)
+
+for x in range(5):
+    print("Range", x)
+
+for x in range(3,8):
+    print("Range between 3 to 8", x)
+
+# 1,2,[3],4,[5],6,[7],8,9
+for x in range(3, 8, 2):
+    print(x)
