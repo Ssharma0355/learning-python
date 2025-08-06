5caa6d11bf904682737b133f1bd57bfa3a758bf8
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-06 07:57:12 +0530

    boolean operators

---
+++ Conditions/Basics.py
@@ -0,0 +1,21 @@
+# it gives in True or False 
+x = 2
+print(x == 2)
+print(x == 3)
+print(x != 2)
+print(x < 2)
+
+# Boolean operators 
+
+# The "and" and "or" boolean operators allow building complex boolean expressions, for example:
+
+name = "Sachin"
+age = 23
+if name == "Sachin" and age == 23:
+    print("Hi ",name)
+
+if name == "Sachin" or age == 24:
+    print("Heyy", name)
+
+if name == "Sachin" and age == 24:
+    print("Heyy", name, "nice to meet you")
