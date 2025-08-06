377084bd6405187aeb4cf4cd76a8c6f77bea1834
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-06 07:45:31 +0530

    split

--- StringFormating/ReverseString.py
+++ StringFormating/ReverseString.py
@@ -19,3 +19,20 @@ print(sampleString.startswith("Sachin"))
 # it checks if the string starts with the same string  
 print(sampleString.endswith("Bakri"))
 #  it checks if the string ends with the same string 
+
+
+
+# This splits the string into a bunch of strings grouped together in a list.
+#  Since this example splits at a space, the first item in the list will be "Hello", and the second will be "world!".
+
+astring = "Hello world!"
+afewwords = astring.split(" ")
+print(afewwords)
+# o/p -> ['Hello', 'world!'] 
+
+aString = "Sachin is a good guy for development"
+makingList = aString.split(" ")
+print(makingList) 
+# o/p -> ['Sachin', 'is', 'a', 'good', 'guy', 'for', 'development']
+print(makingList[3])
+# o/p-> good 
