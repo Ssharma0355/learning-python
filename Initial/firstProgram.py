2bc8734754af2caeb8c997aee34fdebe982f023e
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-19 08:52:44 +0530

    type csting and type conversion

--- Initial/firstProgram.py
+++ Initial/firstProgram.py
@@ -1,16 +1,34 @@
-print("Hey Sachin!!")
+# print("Hey Sachin!!")
 
-# logical operator
-print(not True)
-print(not False)
+# # logical operator
+# print(not True)
+# print(not False)
 
-val1 = True
-# val2 = True
-val2 = False
-print("And operator: ", val1 and val2)
-# both value should be true
+# val1 = True
+# # val2 = True
+# val2 = False
+# print("And operator: ", val1 and val2)
+# # both value should be true
 
-val3 = True
-val4 = False
-print("Or Operator:", val3 or val3)
+# val3 = True
+# val4 = False
+# print("Or Operator:", val3 or val3)
 # either 1 value should be true
+
+
+
+# Type conversion and Type Casting
+
+# pyhton automatic type change (Implecit conversion)
+# Casting is done by manually
+
+a = 3
+b = 3.2
+print(a+b)
+# automtic change 
+
+# Type casting
+c = 23
+d = int("4")
+print(c+d)
+
