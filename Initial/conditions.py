dce139df2d4345f08fcaf5551414f3151679deaf
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-21 09:14:20 +0530

    added conditions

--- Initial/conditions.py
+++ Initial/conditions.py
@@ -1,12 +1,26 @@
 # if-elif-else 
 
-age = 4
+# age = 4
 
-if(age >= 18 and age < 45):
-    print("Can apply for 2 willer")
+# if(age >= 18 and age < 45):
+#     print("Can apply for 2 willer")
 
-elif(age >45):
-    print("Can apply for air plane")
+# elif(age >45):
+#     print("Can apply for air plane")
+
+# else:
+#     print("Abhi tum bache ho")
+
+# Odd or even 
+    
+# num = 7
+num = 12
+# num = "Sachin"
+if(num%2 == 0):
+        print(num," is even")
+
+elif(num%2 !=0):
+        print(num, " is not even")
 
 else:
-    print("Abhi tum bache ho")
+        print(num, " is not even number")
