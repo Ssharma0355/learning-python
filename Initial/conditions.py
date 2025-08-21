1479e248f32fbba266520c1093909e2c85513aa1
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-21 09:07:36 +0530

    conditions in python

---
+++ Initial/conditions.py
@@ -0,0 +1,12 @@
+# if-elif-else 
+
+age = 4
+
+if(age >= 18 and age < 45):
+    print("Can apply for 2 willer")
+
+elif(age >45):
+    print("Can apply for air plane")
+
+else:
+    print("Abhi tum bache ho")
